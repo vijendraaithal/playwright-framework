17b7d626c1c7c492f08da082fe80b2527744c23c
Vijendra Aithal <vijendra.aithal@gmail.com>
2026-02-07 12:07:15 +0530

    added first playwright test

---
+++ tests/demo/first-test.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("Should load home page with correct tile", async ({ page }) => {
+  // Go to Home page
+  await page.goto("https://katalon-demo-cura.herokuapp.com/");
+
+  // Assert if the title is correct
+  await expect(page).toHaveTitle("CURA Healthcare Service");
+
+  // Assert header text
+  await expect(page.locator("\\h1")).toHaveText("CURA Healthcare Service");
+});
